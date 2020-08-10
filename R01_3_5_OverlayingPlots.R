3af60c846d07bfe4c215de55b33a4af456cbbbc8
Shyamnarayan Sankarasubramanian <shyamnarayan93@gmail.com>
2020-08-10 10:31:02 +0200

    Update R01_3_5_OverlayingPlots.R

--- R01_3_5_OverlayingPlots.R
+++ R01_3_5_OverlayingPlots.R
@@ -1,10 +1,3 @@
-# File:    R01_3_5_OverlayingPlots.R
-# Course:  R01: R: An introduction
-# Chapter: 3: Basic graphics
-# Section: 5: Overlaying plots
-# Author:  Barton Poulson, datalab.cc, @bartonpoulson
-# Date:    2016-05-24
-
 # INSTALL AND LOAD PACKAGES ################################
 
 library(datasets)  # Load/unload base packages manually
